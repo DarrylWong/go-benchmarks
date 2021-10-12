ce8224a126a0a45a8de7d867fdb1f969a10915ee
Michael Pratt <mpratt@google.com>
2021-10-12 17:44:09 -0400

    cmd/bench: add benchmark wrapper
    The coordinator is getting support for running the benchmarks in this
    repository. Since the benchmarks and interface are in flux, encoding all
    of the details of running Go tests, bent arguments, etc into the
    coordinator will likely cause churn and frustrating migration issues.
    
    Instead, add cmd/bench which serves as the simply entrypoint for the
    coordinator. The coordinator runs cmd/bench with the GOROOT to test
    (eventually multiple GOROOTs), and this binary takes care of the
    remaining details.
    
    Right now, we just do a basic go test golang.org/x/benchmarks/... and
    simple invocation of bent.
    
    For golang/go#49207
    
    Change-Id: I5c9cf89540cab605c0a64e17af85311d37985c25

---
+++ cmd/bench/gotest.go
@@ -0,0 +1,33 @@
+// Copyright 2021 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"log"
+	"os"
+	"os/exec"
+)
+
+func goTest(goroot string) error {
+	log.Printf("Running Go test benchmarks for GOROOT %s", goroot)
+
+	args := []string{
+		"test",
+		"-v",
+		"-run=none",
+		"-bench=.",
+		"-count=5",
+		"golang.org/x/benchmarks/...",
+	}
+	cmd := exec.Command(goBinary(goroot), args...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Env = []string{
+		"GOROOT="+goroot,
+		"HOME="+os.Getenv("HOME"),
+		"PATH="+os.Getenv("PATH"),
+	}
+	return cmd.Run()
+}
