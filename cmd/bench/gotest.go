d1c349001cae3233a1adaadd3d109bcad7cb963d
Dung Le <dungtuanle@google.com>
2022-11-30 14:16:16 -0500

    cmd/bench: add subrepo directories to run benchmarks on
    For golang/go#53538
    
    Change-Id: I0eb1d2f00ca1977bdd909e0a4bfe4367d388ba16

--- cmd/bench/gotest.go
+++ cmd/bench/gotest.go
@@ -13,9 +13,22 @@ func goTest(tcs []*toolchain) error {
 	for _, tc := range tcs {
 		log.Printf("Running Go test benchmarks for %s", tc.Name)
 		fmt.Printf("toolchain: %s\n", tc.Name)
-		err := tc.Do("test", "-v", "-run=none", "-bench=.", "-count=5", "golang.org/x/benchmarks/...")
+		err := tc.Do("", "test", "-v", "-run=none", "-bench=.", "-count=5", "golang.org/x/benchmarks/...")
 		if err != nil {
 			return fmt.Errorf("error running gotest with toolchain %s: %w", tc.Name, err)
+		}
+	}
+	return nil
+}
+
+func goTestSubrepo(tc *toolchain, subRepo string, dirs []string) error {
+	log.Printf("Running sub-repo benchmarks for %s", subRepo)
+	fmt.Printf("toolchain: %s\n", tc.Name) //toolchain should be baseline or experiment
+	for _, dir := range dirs {
+		err := tc.Do(dir, "test", "-v", "./internal/regtest/bench/", "-count=5")
+		if err != nil {
+			log.Printf("Error: %v", err)
+			return fmt.Errorf("error running sub-repo %s benchmark with toolchain %s in dir %s: %w", subRepo, tc.Name, dir, err)
 		}
 	}
 	return nil
